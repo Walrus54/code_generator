29cfe0e5c55e49158f9f5c1adafc2db41360649c
Walrus54 <evseevalek5ander@yandex.ru>
2026-05-12 13:54:38 +0700

    add abstract class unit

---
+++ src/class_unit.cpp
@@ -0,0 +1,18 @@
+#include "class_unit.h"
+
+#include "text_util.h"
+
+namespace codegen {
+
+ClassUnit::ClassUnit( const std::string& name, Flags flags )
+    : m_name( requireIdentifier( name, "class name" ) ), m_flags( flags ) {}
+
+void ClassUnit::add( const std::shared_ptr< Unit >& unit, Flags flags ) {
+    // FIXME: пока учитываем только модификаторы доступа C++.
+    if( flags == AM_PUBLIC || flags == AM_PROTECTED || flags == AM_PRIVATE ) {
+        unit->setAccessModifier( static_cast< AccessModifier >( flags ) );
+        m_members.push_back( unit );
+    }
+}
+
+} // namespace codegen
