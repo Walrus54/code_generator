58620e4a0be02133561951920dd5676b86cb04d7
Walrus54 <evseevalek5ander@yandex.ru>
2026-05-25 16:47:05 +0700

    add csharp factory

---
+++ src/csharp/csharp_factory.cpp
@@ -0,0 +1,32 @@
+#include "csharp/csharp_factory.h"
+
+#include "csharp/csharp_class_unit.h"
+#include "csharp/csharp_field_unit.h"
+#include "csharp/csharp_method_unit.h"
+#include "csharp/csharp_print_operator_unit.h"
+
+namespace codegen::csharp {
+
+std::shared_ptr< ClassUnit > CSharpFactory::createClass( const std::string& name, Flags flags ) const {
+    return std::make_shared< CSharpClassUnit >( name, flags );
+}
+
+std::shared_ptr< MethodUnit > CSharpFactory::createMethod(
+    const std::string& name, const std::string& returnType, Flags flags ) const {
+    return std::make_shared< CSharpMethodUnit >( name, returnType, flags );
+}
+
+std::shared_ptr< FieldUnit > CSharpFactory::createField(
+    const std::string& name, const std::string& type, Flags flags ) const {
+    return std::make_shared< CSharpFieldUnit >( name, type, flags );
+}
+
+std::shared_ptr< PrintOperatorUnit > CSharpFactory::createPrintOperator( const std::string& text ) const {
+    return std::make_shared< CSharpPrintOperatorUnit >( text );
+}
+
+std::string CSharpFactory::languageName() const {
+    return "C#";
+}
+
+} // namespace codegen::csharp
---
+++ src/csharp/csharp_factory.h
@@ -0,0 +1,23 @@
+#ifndef CODEGEN_SRC_CSHARP_CSHARP_FACTORY_H
+#define CODEGEN_SRC_CSHARP_CSHARP_FACTORY_H
+
+#include "factory.h"
+
+namespace codegen::csharp {
+
+class CSharpFactory : public AbstractFactory {
+public:
+    std::shared_ptr< ClassUnit > createClass(
+        const std::string& name, Flags flags = 0 ) const override;
+    std::shared_ptr< MethodUnit > createMethod(
+        const std::string& name, const std::string& returnType, Flags flags = 0 ) const override;
+    std::shared_ptr< FieldUnit > createField(
+        const std::string& name, const std::string& type, Flags flags = 0 ) const override;
+    std::shared_ptr< PrintOperatorUnit > createPrintOperator(
+        const std::string& text ) const override;
+    std::string languageName() const override;
+};
+
+} // namespace codegen::csharp
+
+#endif // CODEGEN_SRC_CSHARP_CSHARP_FACTORY_H
